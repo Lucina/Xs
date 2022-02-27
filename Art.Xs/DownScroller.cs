445fccabce3590d0cae75b8d44980133042111a8
Lucina <10523871+Lucina@users.noreply.github.com>
2022-02-27 04:15:51 -0800

    Import Art.Xs for 0.39.0

---
+++ Art.Xs/DownScroller.cs
@@ -0,0 +1,93 @@
+﻿using Xs;
+
+namespace Art.Xs
+{
+    /// <summary>
+    /// Represents a periodic page-bottom scroll action on an <see cref="XsClient"/>.
+    /// </summary>
+    public sealed class DownScroller : IDisposable
+    {
+        private readonly XsClient _client;
+        private readonly int _msDelay;
+        private CancellationTokenSource _cts;
+        private CancellationToken _ct;
+        private bool _disposed;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="DownScroller"/>.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="msDelay"></param>
+        public DownScroller(XsClient client, int msDelay)
+        {
+            (_client, _msDelay, _cts) = (client, msDelay, new CancellationTokenSource());
+            _ct = _cts.Token;
+        }
+
+        /// <summary>
+        /// Start scrolling.
+        /// </summary>
+        public void Start()
+        {
+            NotDisposed();
+            Task.Factory.StartNew(Execute(_ct), TaskCreationOptions.LongRunning);
+        }
+
+        /// <summary>
+        /// Stop scrolling.
+        /// </summary>
+        public void Stop()
+        {
+            NotDisposed();
+            _cts.Cancel();
+            _cts.Dispose();
+            _cts = new CancellationTokenSource();
+            _ct = _cts.Token;
+        }
+
+        private Func<Task> Execute(CancellationToken ct) => async () =>
+        {
+            while (true)
+            {
+                ct.ThrowIfCancellationRequested();
+                await Task.Delay(_msDelay, ct);
+                await _client.ExecuteJsAsync("window.scrollTo(0, document.body.scrollHeight);");
+            }
+        };
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    _cts.Cancel();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _disposed = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~DownScroller()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+        }
+
+        private void NotDisposed()
+        {
+            if (_disposed) throw new ObjectDisposedException(nameof(DownScroller));
+        }
+    }
+}
---
+++ Art.Xs/XsArtifactTool.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using AngleSharp.Dom;
+using Art.Html;
+using Xs;
+
+namespace Art.Xs;
+
+/// <summary>
+/// Represents an artifact tool that uses <see cref="XsClient"/>.
+/// </summary>
+public abstract class XsArtifactTool : HtmlArtifactTool
+{
+    /// <summary>
+    /// Asynchronously creates a <see cref="XsClient"/>.
+    /// </summary>
+    /// <returns>Task rturning an Xs client.</returns>
+    public static Task<XsClient> CreateXsClientAsync() => XsManager.CreateClientAsync();
+
+    #region Html overloads
+
+    /// <summary>
+    /// Opens a new document loaded from the provided <see cref="XsClient"/>.
+    /// </summary>
+    /// <param name="xs">Client with active page.</param>
+    /// <returns>Task returning the loaded document.</returns>
+    public async Task<IDocument> OpenAsync(XsClient xs)
+        => await OpenStringAsync(await xs.GetContentAsync()).ConfigureAwait(false);
+
+    #endregion
+
+    #region HttpClient syncing
+
+    /// <summary>
+    /// Synchronizes settings from an <see cref="XsClient"/> to current <see cref="HttpClient"/>.
+    /// </summary>
+    /// <param name="xs">Xs client.</param>
+    /// <returns>Task.</returns>
+    public async Task SyncToHttpClientAsync(XsClient xs)
+    {
+        (List<Cookie> cookies, string ua) = await xs.ExecuteAsync(
+            async w => ((await w.CookieManager.GetCookiesAsync("")).Select(c => c.ToSystemNetCookie()).ToList(), w.Settings.UserAgent));
+        foreach (Cookie c in cookies)
+            CookieContainer.Add(c);
+        HttpClient.DefaultRequestHeaders.UserAgent.TryParseAdd(ua);
+    }
+
+    /// <summary>
+    /// Synchronizes settings from current <see cref="HttpClient"/> to an <see cref="XsClient"/>.
+    /// </summary>
+    /// <param name="xs">Xs client.</param>
+    /// <returns>Task.</returns>
+    public async Task SyncFromHttpClientAsync(XsClient xs)
+    {
+        List<Cookie> cookies = CookieContainer.GetAllCookies().ToList();
+        string ua = HttpClient.DefaultRequestHeaders.UserAgent.ToString();
+        await xs.ExecuteAsync(w =>
+        {
+            foreach (Cookie c in cookies)
+                w.CookieManager.AddOrUpdateCookie(w.CookieManager.CreateCookieWithSystemNetCookie(c));
+            w.Settings.UserAgent = ua;
+        });
+    }
+
+    #endregion
+}
